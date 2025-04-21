519c6c45f6f7eb0190decd0048eabd5211abbec8
5upportPark <5upportpark7@gmail.com>
2025-04-21 12:00:26 +0900

    init

---
+++ src/main/java/com/pjw/tickgettinig/TickgettinigApplication.java
@@ -0,0 +1,13 @@
+package com.pjw.tickgettinig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickgettinigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickgettinigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pjw/tickgettinig/TickgettinigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjw.tickgettinig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickgettinigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
